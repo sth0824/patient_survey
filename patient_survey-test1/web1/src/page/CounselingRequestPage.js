cb4aaa783f41ed2a7c91f891e196ea443a84e085
SungTaeHwan <165628425+sth0824@users.noreply.github.com>
2025-09-15 16:38:24 +0900

    Delete patient_survey-test1 directory

--- patient_survey-test1/web1/src/page/CounselingRequestPage.js
+++
@@ -1,9 +0,0 @@
-// src/pages/CounselingRequestPage.jsx
-import React from "react";
-import CounselingRequestForm from "../component/CounselingRequestForm";
-
-const CounselingRequestPage = () => {
-  return <CounselingRequestForm />;
-};
-
-export default CounselingRequestPage;
